474b01c8419093b1ca192f4f1f2cb64e3e700a43
Shamp07 <blog.shamp@gmail.com>
2019-03-11 17:27:53 +0900

    Added Encrypted Datasource username,password

---
+++ src/main/java/kr/co/upcoding/encrypt/EncryptionEnvironmentPostProcessor.java
@@ -0,0 +1,31 @@
+package kr.co.upcoding.encrypt;
+
+import org.apache.tomcat.jni.OS;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.PropertiesPropertySource;
+import kr.co.upcoding.encrypt.AES256Cipher;
+import java.util.Properties;
+
+public class EncryptionEnvironmentPostProcessor implements EnvironmentPostProcessor {
+
+    @Override
+    public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+        Properties props = new Properties();
+        try {
+            // 암호화 한 문자열 보기 ( datasource의 정보를 하단 인자에 넣어주세요.)
+            //System.out.println("encrypted spring.datasource.password : "+AES256Cipher.getInstance().AES_Encode("username"));
+            //System.out.println("encrypted spring.datasource.username : "+AES256Cipher.getInstance().AES_Encode("password"));
+            props.put("spring.datasource.password", AES256Cipher.getInstance().AES_Decode(environment.getProperty("spring.datasource.password")));
+            props.put("spring.datasource.username", AES256Cipher.getInstance().AES_Decode(environment.getProperty("spring.datasource.username")));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        environment.getPropertySources().addFirst(new PropertiesPropertySource("myProps", props));
+    }
+
+
+
+}
